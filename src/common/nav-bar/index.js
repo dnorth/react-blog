590178c359ac6367295cd4903decbfb1b9de100b
Danny North <lds.north@gmail.com>
2017-08-10 21:38:52 -0500

    replace cool nav with semi-lame nav until I can figure out how to get the kinks out of cool nav. That thing was giving me a huge headache

--- src/common/nav-bar/index.js
+++ src/common/nav-bar/index.js
@@ -1,45 +1,42 @@
 import autobind from 'autobind-decorator'
-import classnames from 'classnames'
-import throttle from 'lodash.throttle'
 import React from 'react'
+import throttle from 'lodash.throttle'
 import { Sticky } from 'react-sticky'
 
 import css from './index.css'
 
+
 @autobind
 export default class NavigationBar extends React.Component {
   state = {
-    shouldDisplayNav: false
+    isAtTop: true
   }
-
+  
   calculateDisplayNav() {
     this.setState({
-      shouldDisplayNav: this.prevScroll >= window.scrollY && window.scrollY !== 0
+      isAtTop: window.scrollY === 0
     })
-
-    this.prevScroll = window.scrollY
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', throttle(this.calculateDisplayNav, 200))
   }
 
-  componentDidMount(){
-      window.addEventListener('scroll', throttle(this.calculateDisplayNav, 200));
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.caluclateDisplayNav)
   }
-
-  componentWillUnmount(){
-      window.removeEventListener('scroll', this.calculateDisplayNav);
-  }
-
+  
   render() {
-    const classname = classnames(css.stickyContent, this.state.shouldDisplayNav ? css.displaySticky : css.hideSticky)
+
+    const stickyClass = this.state.isAtTop ? css.content : css.stickyContent
 
     return (
       <div>
-        <div className={css.content}>Sticky Sticky</div>
-
         <Sticky>
           {
             props => {
               return (
-                <div className={classname} style={ {...props.style, transform: ''}}>
+                <div className={stickyClass} style={ {...props.style, transform: ''} }>
                   {this.props.children}
                 </div>
               )
@@ -48,5 +45,5 @@ export default class NavigationBar extends React.Component {
         </Sticky>
       </div>
     )
-  } 
+  }
 }
--- src/dash/index.js
+++ src/dash/index.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <ChromeLayout>
-        <div className={css.body}>
+        <div className={css.content}>
           <ReactMarkdown source={helloWorld}
                         renderers={ { CodeBlock } } />
         </div>
