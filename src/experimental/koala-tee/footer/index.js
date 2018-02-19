372aa603800839680af589585861b785d86ac886
Danny North <lds.north@gmail.com>
2018-02-19 03:23:16 -0700

    Add footer and body to koala tee layout

---
+++ src/experimental/koala-tee/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import css from './index.css'
+
+const Footer = () => (
+    <div className={css.footerGrid}>
+        hello
+    </div>
+)
+
+export default Footer
--- src/experimental/koala-tee/index.js
+++ src/experimental/koala-tee/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Footer from './footer'
 import Header from './header'
 
 import css from './index.css'
@@ -7,6 +8,8 @@ import css from './index.css'
 const KoalaTee = () => (
     <div className={css.koalaGrid}>
         <Header />
+        <span>Content</span>
+        <Footer />
     </div>
 )
 
